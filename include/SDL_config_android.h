46b9b491273c1ee9fc3ec45b3561c43c15451242
Jeremy Faivre <contact@jeremyfa.com>
2022-04-30 12:26:34 +0200

    Fixes to make newer SDL work on Android (disable hidapi on this platform)

--- include/SDL_config_android.h
+++ include/SDL_config_android.h
@@ -146,9 +146,12 @@
 #define SDL_AUDIO_DRIVER_AAUDIO     1
 #define SDL_AUDIO_DRIVER_DUMMY  1
 
+/* Disable HIDAPI */
+#define SDL_HIDAPI_DISABLED 1
+
 /* Enable various input drivers */
 #define SDL_JOYSTICK_ANDROID    1
-#define SDL_JOYSTICK_HIDAPI     1
+#define SDL_JOYSTICK_HIDAPI     0
 #define SDL_JOYSTICK_VIRTUAL    1
 #define SDL_HAPTIC_ANDROID  1
 
